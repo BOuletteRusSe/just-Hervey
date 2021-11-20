3d20caee1bdf863d97dff316e755ec01d6d76b18
Enz0lanterne <79151100+Enz0lanterne@users.noreply.github.com>
2021-11-20 15:33:16 +0100

    v. 3.4.4

--- dist/client_commands/activity.py
+++ dist/client_commands/activity.py
@@ -5,4 +5,4 @@ async def ChangeActivity(c):
     while True:
         try: await c.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=str(choice([word.strip() for word in open("assets/texts/boule.txt", encoding="utf-8")]))))
         except: pass
-        await asyncio.sleep(10)
+        await asyncio.sleep(20)
