4d9eacc319a57df7453e621ca9271eb587c8ae34
Enz0lanterne <79151100+Enz0lanterne@users.noreply.github.com>
2023-02-14 18:41:38 +0100

    v. 3.9.2

--- dist/client.py
+++ dist/client.py
@@ -59,7 +59,7 @@ with open(r"assets\player_data.json") as data:
         def __init__(self):
             intents = discord.Intents().all()
             self.bot = commands.Bot(command_prefix="c!", description="#Nazomazochiste", intents=intents)
-            self.client_version = '3.9.1'
+            self.client_version = '3.9.2'
             self.key = open(".PRIVATE/key.key", "rb").read()
             f = Fernet(self.key)
             with open(".PRIVATE/token", "rb") as file: encrypted_data = file.read()
--- dist/client_commands/inventory.py
+++ dist/client_commands/inventory.py
@@ -1,4 +1,4 @@
-import discord, json, re
+import discord, json, re, asyncio
 from assets.items_price import item_shop_price, item_shop_price_2
 
 
@@ -69,46 +69,80 @@ async def Inventory(ctx, equip, c):
     for rank in data[id]["Inventory"]["P Rank"]:
         ranks += f"{str(item_shop_price[rank]['Name'])} ({rank}) | "
     
-    inventory_embed = discord.Embed(title="⚔ INVENTAIRE ⚔", description="Ici, vous pouvez voir tout ce que vous avez a disposition dans votre inventaire.", color=0x1e4843)
-    inventory_embed.set_author(name=user, icon_url=user.avatar_url)
-    inventory_embed.add_field(name="🛠 • Objet(s) Équipé(s) :", value=e_items)
-    inventory_embed.add_field(name="🎭 • Grade :", value=item_shop_price[data[id]["Inventory"]["Rank"]]["Name"], inline=True)
-    inventory_embed.add_field(name="Liste des Objets :", value=items, inline=True)
-    inventory_embed.add_field(name="Liste des Grades :", value=ranks, inline=True)
-    inventory_embed.add_field(name="🎟 • Tickets :", value=data[id]["Ticket"], inline=True)
-    inventory_embed.add_field(name=":pick: • Nombre de Pioches Max :", value=data[id]['Inventory']['Item Limit'], inline=True)
-    
-    InventoryCheckNone("<:debrit:882240995717156874> • Debrit :", "Debrit", id, data, inventory_embed)
-    InventoryCheckNone("<:stone:882241850965118978> • Pierre :", "Stone", id, data, inventory_embed)
-    InventoryCheckNone("<:iron:881949148876783646> • Fer :", "Iron", id, data, inventory_embed)
-    InventoryCheckNone("<:silver:881958476287459408> • Argent :", "Silver", id, data, inventory_embed)
-    InventoryCheckNone("<:gold:881954591774736395> • Or :", "Gold", id, data, inventory_embed)
-    InventoryCheckNone("<:diamond:881949161753309194> • Diamant :", "Diamond", id, data, inventory_embed)
-    InventoryCheckNone("<:platinium:881964089667121202> • Platine :", "Platinium", id, data, inventory_embed)
-    InventoryCheckNone("<:rubis:881960449938186240> • Rubis :", "Rubis", id, data, inventory_embed)
-    InventoryCheckNone("<:saphir:881963269424824370> • Saphir :", "Saphir", id, data, inventory_embed)
-    InventoryCheckNone("<:emerald:881983813532647434> • Emeraude :", "Emerald", id, data, inventory_embed)
-    InventoryCheckNone("<:cobalt:882231543358165002> • Cobalt :", "Cobalt", id, data, inventory_embed)
-    InventoryCheckNone("<:amethist:881985189511839744> • Améthyste :", "Amethist", id, data, inventory_embed)
-    InventoryCheckNone("<:grenat:881962367037087785> • Grenat :", "Grenat", id, data, inventory_embed)
-    InventoryCheckNone("<:turquoise:882235863499686028> • Turquoise :", "Turquoise", id, data, inventory_embed)
-    InventoryCheckNone("<:obsidian:882238111818612736> • Obsidienne :", "Obsidian", id, data, inventory_embed)
-    InventoryCheckNone("<:randomite:881964979639709748> • Randomite :", "Randomite", id, data, inventory_embed)
-    InventoryCheckNone("<:mercury:882233112283742218> • Mercure :", "Mercury", id, data, inventory_embed)
-    InventoryCheckNone("<:magmastone:881968174025801798> • Pierre Magmatique :", "Magma Stone", id, data, inventory_embed)
-    InventoryCheckNone("<:fossil:881977977087336498> • Fossille :", "Fossil", id, data, inventory_embed)
-    InventoryCheckNone("<:sacredstone:882234999145922621> • Pierre Sacrée :", "Sacred Stone", id, data, inventory_embed)
-    InventoryCheckNone("<:coke:882232188177903626> • Coke :", "Coke", id, data, inventory_embed)
-    InventoryCheckNone("<:fluorite:882237334848933918> • Fluorite", "Fluorite", id, data, inventory_embed)
-    InventoryCheckNone("<:jade:882239569653792808> • Jade", "Jade", id, data, inventory_embed)
-    InventoryCheckNone("<:coal:882226330110926908> • Charbon :", "Coal",id, data, inventory_embed)
-    InventoryCheckNone("<:cooper:882228895192076339> • Cuivre :", "Cooper", id, data, inventory_embed)
-    InventoryCheckNone("<:magnetite:1075060513593110689> • Magnétite :", "Magnetite", id, data, inventory_embed)
-    inventory_embed.set_footer(text=f"c!inventory equip item/rank 'nombre' pour équiper une objet ou un grade. (Vous pouvez équiper jusqu'à {data[id]['Inventory']['Item Limit']} item(s) à la fois.)")
-
+    def DefaultEmbed(page):
+        inventory_embed = discord.Embed(title="⚔ INVENTAIRE ⚔", description="Ici, vous pouvez voir tout ce que vous avez a disposition dans votre inventaire.", color=0x1e4843)
+        inventory_embed.set_author(name=user, icon_url=user.avatar_url)
+        inventory_embed.add_field(name="🛠 • Objet(s) Équipé(s) :", value=e_items)
+        inventory_embed.add_field(name="🎭 • Grade :", value=item_shop_price[data[id]["Inventory"]["Rank"]]["Name"], inline=True)
+        inventory_embed.add_field(name="Liste des Objets :", value=items, inline=True)
+        inventory_embed.add_field(name="Liste des Grades :", value=ranks, inline=True)
+        inventory_embed.add_field(name="🎟 • Tickets :", value=data[id]["Ticket"], inline=True)
+        inventory_embed.add_field(name=":pick: • Nombre de Pioches Max :", value=data[id]['Inventory']['Item Limit'], inline=True)
+        inventory_embed.set_footer(text=f"c!inventory equip item/rank 'nombre' pour équiper une objet ou un grade. (Vous pouvez équiper jusqu'à {data[id]['Inventory']['Item Limit']} item(s) à la fois.)")
+        
+        if page == 0:
+            InventoryCheckNone("<:debrit:882240995717156874> • Debrit :", "Debrit", id, data, inventory_embed)
+            InventoryCheckNone("<:stone:882241850965118978> • Pierre :", "Stone", id, data, inventory_embed)
+            InventoryCheckNone("<:iron:881949148876783646> • Fer :", "Iron", id, data, inventory_embed)
+            InventoryCheckNone("<:silver:881958476287459408> • Argent :", "Silver", id, data, inventory_embed)
+            InventoryCheckNone("<:gold:881954591774736395> • Or :", "Gold", id, data, inventory_embed)
+            InventoryCheckNone("<:diamond:881949161753309194> • Diamant :", "Diamond", id, data, inventory_embed)
+            InventoryCheckNone("<:platinium:881964089667121202> • Platine :", "Platinium", id, data, inventory_embed)
+            InventoryCheckNone("<:rubis:881960449938186240> • Rubis :", "Rubis", id, data, inventory_embed)
+            InventoryCheckNone("<:saphir:881963269424824370> • Saphir :", "Saphir", id, data, inventory_embed)
+            InventoryCheckNone("<:emerald:881983813532647434> • Emeraude :", "Emerald", id, data, inventory_embed)
+            InventoryCheckNone("<:cobalt:882231543358165002> • Cobalt :", "Cobalt", id, data, inventory_embed)
+            InventoryCheckNone("<:amethist:881985189511839744> • Améthyste :", "Amethist", id, data, inventory_embed)
+            InventoryCheckNone("<:grenat:881962367037087785> • Grenat :", "Grenat", id, data, inventory_embed)
+            InventoryCheckNone("<:turquoise:882235863499686028> • Turquoise :", "Turquoise", id, data, inventory_embed)
+            InventoryCheckNone("<:obsidian:882238111818612736> • Obsidienne :", "Obsidian", id, data, inventory_embed)
+            InventoryCheckNone("<:randomite:881964979639709748> • Randomite :", "Randomite", id, data, inventory_embed)
+            InventoryCheckNone("<:mercury:882233112283742218> • Mercure :", "Mercury", id, data, inventory_embed)
+            InventoryCheckNone("<:magmastone:881968174025801798> • Pierre Magmatique :", "Magma Stone", id, data, inventory_embed)
+            InventoryCheckNone("<:fossil:881977977087336498> • Fossille :", "Fossil", id, data, inventory_embed)
+            
+        elif page == 1:
+            InventoryCheckNone("<:sacredstone:882234999145922621> • Pierre Sacrée :", "Sacred Stone", id, data, inventory_embed)
+            InventoryCheckNone("<:coke:882232188177903626> • Coke :", "Coke", id, data, inventory_embed)
+            InventoryCheckNone("<:fluorite:882237334848933918> • Fluorite", "Fluorite", id, data, inventory_embed)
+            InventoryCheckNone("<:jade:882239569653792808> • Jade", "Jade", id, data, inventory_embed)
+            InventoryCheckNone("<:coal:882226330110926908> • Charbon :", "Coal",id, data, inventory_embed)
+            InventoryCheckNone("<:cooper:882228895192076339> • Cuivre :", "Cooper", id, data, inventory_embed)
+            InventoryCheckNone("<:magnetite:1075060513593110689> • Magnétite :", "Magnetite", id, data, inventory_embed)
+        
+        return inventory_embed
+        
+    max_page = 1
+    min_page = 0
+    page = min_page
+    __embed__ = DefaultEmbed(page)
+    mess = await ctx.reply(embed=__embed__)
+    await mess.add_reaction("⬅")
+    await mess.add_reaction("➡")
+        
     if not equip:
         if not data[id]["Inventory"] is None:
-            await ctx.reply(embed=inventory_embed)
+            w = True
+            while w:
+                
+                def CheckEmoji(reaction, user):
+                    return ctx.message.author == user and mess.id == reaction.message.id and (str(reaction.emoji) in ["⬅", "➡"])
+
+                try:
+                    reaction, user = await c.bot.wait_for("reaction_add", timeout=60, check=CheckEmoji)
+                except asyncio.TimeoutError:
+                    w = False
+                else:
+                    if str(reaction.emoji) == "⬅":
+                        if page != min_page:
+                            page -= 1               
+                    elif str(reaction.emoji) == "➡":
+                        if page != max_page:
+                            page += 1
+                            
+                    await mess.edit(embed=DefaultEmbed(page))
+                            
+            
         else:
             await ctx.reply("Veuillez vous inscrire avec la commande **c!sign** !")
 
